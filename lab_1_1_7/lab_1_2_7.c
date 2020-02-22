6f5e0218cdb53ff542afb8ad2813aa9a6c03e863
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-22 14:21:09 +0300

    Upload New File

---
+++ lab_1_1_7/lab_1_2_7.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+float series_estimate(float x, float eps)
+{
+    float series_part = x;
+    float series_sum = x;
+    int n = 1,m = 2;
+
+    while (fabsf(series_part) > eps)
+    {
+        series_part *= ((n * pow(x, 2 * n + 1)) / n);
+        series_sum += series_part;
+        m += 2;
+        n += 2;
+    }
+    return series_sum;
+}
+int main()
+{
+    float sum_s,eps;
+    float x;
+
+    printf("enter x and eps\n");
+    scanf("%f%f",&x,&eps);
+    if(x > 1 || x < -1)
+    {
+        printf("Epsilon error or x value invalid");
+        return;
+    }
+    sum_s = series_estimate(x,eps);
+    printf("%f",sum_s);
+}
