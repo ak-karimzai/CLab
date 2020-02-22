e60796589c64a7304d2e5be76c008df92aa7cf70
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-22 14:16:52 +0300

    Upload New File

---
+++ lab_1_1_3/lab_1_1_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+float sum_temperator()
+{
+    float v1,t1,v2,t2,v,T;
+    scanf("%f%f%f%f",&v1,&t1,&v2,&t2);
+    v = v1 + v2;
+    T = ((t1 * v1) + (t2 * v2)) / v;
+    return T;
+}
+int main()
+{
+    float x;
+    x = sum_temperator();
+    printf("%.4f",x);
+}
