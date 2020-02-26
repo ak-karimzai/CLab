74f59416a6abb165800b0cb4128fb3e0258727a5
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-26 23:03:04 +0300

    Update main.c

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -4,29 +4,29 @@
 
 int main()
 {
-    float x1,y1,x2,y2,x3,y3,ab,ac,bc,p,xp,yp,sab,sac,sbc;
-    float s = 0,sum_all;
+    float x1, y1, x2, y2, x3, y3, ab, ac, bc, p, xp, yp, sab, sac, sbc;
+    float s = 0, sum_all;
     printf("Enter first point's\n");
-    scanf("%f%f",&x1,&y1);
+    scanf("%f%f", &x1, &y1);
     printf("Enter second point's\n");
-    scanf("%f%f",&x2,&y2);
+    scanf("%f%f", &x2, &y2);
     printf("Enter third point's\n");
-    scanf("%f%f",&x3,&y3);
-    ab = sqrt(pow(x2-x1,2) + pow(y2 - y1,2));
-    ac = sqrt(pow(x3-x1,2) + pow(y3 - y1,2));
+    scanf("%f%f", &x3, &y3);
+    ab = sqrt(pow(x2 - x1,2) + pow(y2 - y1,2));
+    ac = sqrt(pow(x3 - x1,2) + pow(y3 - y1,2));
     bc = sqrt(pow(x3 - x2,2) + pow(y3 - y2,2));
     p = (ab + ac + bc) / 2;
     s = sqrt(p * (p - ab) * (p - ac) * (p - bc));
 
     printf("Enter coordinate of point's\n");
-    scanf("%f%f",&xp,&yp);
+    scanf("%f%f", &xp, &yp);
 
-    sab = (x1 * (y2 - yp) + x2 * (yp - y1) + xp * (y1 - y2))/2.0;
-    sbc = (xp * (y2 - y3) + x2 * (y3 - yp) + x3 * (yp - y2))/2.0;
-    sac = (x1 * (yp - y3) + xp * (y3 - y1) + x3 * (y1 - yp))/2.0;
+    sab = (x1 * (y2 - yp) + x2 * (yp - y1) + xp * (y1 - y2)) / 2.0;
+    sbc = (xp * (y2 - y3) + x2 * (y3 - yp) + x3 * (yp - y2)) / 2.0;
+    sac = (x1 * (yp - y3) + xp * (y3 - y1) + x3 * (y1 - yp)) / 2.0;
 
     sum_all = abs((abs(sab) + abs(sbc) + abs(sac)) - s);
 
-    if(sum_all < (1e-1)) printf("point in triangle.\n");
+    if(sum_all < (1e - 1)) printf("point in triangle.\n");
     else printf("point is'nt triangle\n");
 }
