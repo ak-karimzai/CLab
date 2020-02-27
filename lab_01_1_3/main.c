7c7fed3bc7ba226a3ce1e605789150c3834f561b
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-27 08:28:09 +0300

    Update main.c

--- lab_01_1_3/main.c
+++ lab_01_1_3/main.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 #include<math.h>
 
-float sum_temperator()
+void sum_temperator()
 {
-    float v1, t1, v2, t2, v, T;
+    float v1, t1, v2, t2, v, t;
     scanf("%f%f%f%f", &v1, &t1, &v2, &t2);
     v = v1 + v2;
-    T = ((t1 * v1) + (t2 * v2)) / v;
-    return T,v;
+    t = ((t1 * v1) + (t2 * v2)) / v;
+    printf("%.4f  %.4f", v, t);
+}
 }
 int main()
 {
-    float x, y;
-    x, y = sum_temperator();
-    printf("%.4f  %.4f", y, x);
+    sum_temperator();
 }
