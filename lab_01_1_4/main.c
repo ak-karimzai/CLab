1896dda4764ba89b3a7b97b25cda42beb4a5a341
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-27 08:32:08 +0300

    Update main.c

--- lab_01_1_4/main.c
+++ lab_01_1_4/main.c
@@ -5,8 +5,8 @@ void find_floor()
 {
     int x, e, f;
     scanf("%d", &x);
-    E = (x - 1) / 36 + 1;
-    f = ((( x - (e - 1) * 36) - 1) / 4) + 1;
+    e = (x - 1) / 36 + 1;
+    f = ((x - (e - 1) * 36) - 1) / 4 + 1;
     printf("%d %d", e, f);
     
 }
