9fbec170b2807c4debc6fed6057c0ff2a0c9a770
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-29 22:18:52 +0300

    Update main.c

--- lab_01_1_2/main.c
+++ lab_01_1_2/main.c
@@ -1,17 +1,11 @@
 #include<stdio.h>
 #include<math.h>
 
-float surface_trapezoid()
+int main()
 {
     float d1, d2, s, a;
     scanf("%f%f%f", &d1, &d2, &a);
     a = a * 3.14 / 180;
     s = ((d1 + d2) / 2) * (sin(a) / cos(a) * (d2 - d1) / 2);
-    return s;
-}
-int main()
-{
-    float x;
-    x = surface_trapezoid();
-    printf("%.4f", x);
+    printf("%.4f", s);
 }
