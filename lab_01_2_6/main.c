3c2c62858e6fc8743dca1099768d610fa120b24e
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 00:47:28 +0300

    Update main.c

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -6,7 +6,7 @@ int main()
     float x1, y1, x2, y2, x3, y3, ab, ac, bc, p, xp, yp, sab, sac, sbc, r;
     float s = 0, sum_all;
     printf("Enter coordinate of point's and then coordinates of triangle: ");
-    r = scanf("%f%f%f%f%f%f%f%f", &xp, &yp, &x1, &y1, &x2, &y2, &x3, &y3);
+    r = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3, &xp, &yp);
     ab = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     ac = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
     bc = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
@@ -18,7 +18,12 @@ int main()
     sum_all = fabsf((fabsf(sab) + fabsf(sbc) + fabsf(sac)) - s);
     if (r == 8)
     {
-        if (sum_all < (1e-1))
+        if ((xp == x1 && yp == y1) || (xp == x2 && yp == y2) || (xp == x3 && yp == y3))
+        {
+            printf("point in coordinates");
+            return 1;
+        }
+        else if (sum_all < (1e-1))
         {
             printf("point in triangle.");
             return 0;
@@ -26,13 +31,12 @@ int main()
         else 
         {
             printf("point not in triangle.");
-            return 1;
+            return 2;
         }
     }
     else
     {
         printf("input error");
-        return 1;
+        return -1;
     }
-    
 }
