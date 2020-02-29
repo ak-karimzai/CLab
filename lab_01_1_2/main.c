e0537ec7a6d87cc6f5bbaea41c69f4c6dc8b50aa
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-02-29 22:15:31 +0300

    Update main.c

--- lab_01_1_2/main.c
+++ lab_01_1_2/main.c
@@ -6,7 +6,7 @@ float surface_trapezoid()
     float d1, d2, s, a;
     scanf("%f%f%f", &d1, &d2, &a);
     a = a * 3.14 / 180;
-    s = 1 / 2 * d1 * d2 * sin(a);
+    s = ((d1 + d2) / 2) * (sin(a) / cos(a) * (d2 - d1) / 2);
     return s;
 }
 int main()
