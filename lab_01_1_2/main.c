7b946060fdb6a9ea446403c3ef9111081f8e5d58
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 00:59:57 +0300

    Update main.c

--- lab_01_1_2/main.c
+++ lab_01_1_2/main.c
@@ -6,7 +6,7 @@ int main()
     float a, b, s, d, h;
     scanf("%f%f%f", &a, &b, &d);
     d = d * 3.14 / 180;
-    h = (a - b) * sin(d) / (2 * cos(d));
+    h = (a - b) * sinf(d) / (2 * cosf(d));
     s = (a + b) * h / 2;
     printf("%.4f", s);
 }
