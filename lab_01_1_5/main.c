f6d92e7abf998272d21603c541e2d9ddf4c103db
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 19:31:40 +0300

    Update main.c

--- lab_01_1_5/main.c
+++ lab_01_1_5/main.c
@@ -2,27 +2,29 @@
 
 int main()
 {
-   int a,d,r,q, rc;
-   printf("Enter the values\n");
-   rc = scanf("%d%d", &a, &d);
-   r = a, q = 0;
-   if (a < 0 || b < 0)
-   {
-      printf("incorrext input");
-      return 2;
-   }
-   if (rc == 2)
-      while(!(r < d))
-      {
-         r = r - d;
-         q++;
-      }
-      printf("%d", q);
-      printf("%d", r);
-      return 0;
-   else
-   {
-      printf("incorrect input");
-      return 1;
-   }
+    int a,d,r,q, rc;
+    printf("Enter the values\n");
+    rc = scanf("%d%d", &a, &d);
+    r = a, q = 0;
+    if (a < 0 || d < 0)
+    {
+        printf("incorrext input");
+        return 2;   
+    }
+    if (rc == 2)
+    {
+        while(!(r < d))
+        {
+            r = r - d;
+            q++;
+        }
+        printf("%d", q);
+        printf("%d", r);
+        return 0;
+    }
+    else
+    {
+        printf("incorrect input");
+        return 1;
+    }
 }
