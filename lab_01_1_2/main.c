2dca5c0b43fa797c640c02f1c420c051dfcbf1e1
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 09:19:04 +0300

    Update main.c

--- lab_01_1_2/main.c
+++ lab_01_1_2/main.c
@@ -1,12 +1,25 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+#define PI_DEG 180
+
+#define OK 0
 
 int main()
 {
-    float a, b, s, d, h;
-    scanf("%f%f%f", &a, &b, &d);
-    d = d * 3.14 / 180;
-    h = (a - b) * sinf(d) / (2 * cosf(d));
-    s = (a + b) * h / 2;
-    printf("%.4f", s);
+    double a, b, alpha;
+    double square;
+    double h, p;
+
+    printf("Input a, b, alpha: ");
+    scanf("%lf%lf%lf", &a, &b, &alpha);
+
+    p = fabs(a - b) / 2;
+    h = p * tan(alpha * PI / PI_DEG);
+    square = (a + b) / 2 * h;
+
+    printf("Total square of shape: S = %-12.5f", square);
+
+    return OK;
 }
