672cb36c4714ed75e023141096dd44cf40660c44
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 18:24:21 +0300

    Update main.c

--- lab_01_1_5/main.c
+++ lab_01_1_5/main.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 
-int pow(int m, int n)
+int Pow(int m, int n)
 {
-   if (n == 0)
+    if (n == 0)
+    {
       return 1;
-   return m * pow(m, n - 1);
+    }
+    return m * Pow(m, n - 1);
 }
 int main()
 {
-   int a, n, r;
-   r = scanf("%d%d", &a, &n);
-   if (n < 0)
-   {
+    int a, n, r;
+    r = scanf("%d%d", &a, &n);
+    if (n < 0)
+    {
       printf("incorect input");
       return 2;
-   }
-   if(r == 2)
-   {
-      printf("%d", pow(a, n));
+    }
+    if (r == 2)
+    {
+      printf("%d", Pow(a, n));
       return 0;
-   }
-   else
-   {
+    }
+    else
+    {
       printf("input error");
       return 1;
-   }
+    }
 }
