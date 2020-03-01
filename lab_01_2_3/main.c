c47806d0cb66011f6c65cb08a66abdd6b20e0faa
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 19:02:39 +0300

    Update main.c

--- lab_01_2_3/main.c
+++ lab_01_2_3/main.c
@@ -6,6 +6,6 @@ int main()
     float r1, r2, r3, r;
     scanf("%f%f%f", &r1, &r2, &r3);
     r = (r1 * r2 * r3) / (r1 * r2 + r2 * r3 + r3 * r1);
-    printf("%f", R);
+    printf("%f", r);
     return 0;
 }
