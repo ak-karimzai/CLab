3d2609cccadddd78aad22db0a70ecff55bf55d61
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-01 16:43:34 +0300

    Update main.c

--- lab_01_2_2/main.c
+++ lab_01_2_2/main.c
@@ -6,7 +6,7 @@ int main()
     float x1, y1, x2, y2, x3, y3, ab, ac, bc, p, xp, yp, sab, sac, sbc, r;
     float s = 0, sum_all;
     printf("Enter coordinate of point's and then coordinates of triangle: ");
-    r = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3, &xp, &yp);
+    r = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3);
     ab = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     ac = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
     bc = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
