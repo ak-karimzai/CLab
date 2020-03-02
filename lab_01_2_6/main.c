7c5a10144c08e69a923334ddc016c110056193f3
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-02 11:21:00 +0300

    Update main.c

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -26,19 +26,16 @@ int main(void)
     int rc = 0, flag;
     float x1, y1, x2, y2, x3, y3;
     float xp, yp;
-
+    
     rc = string_hello('A', &x1, &y1);
     if (rc)
         return print_error('A');
-
     rc = string_hello('B', &x2, &y2);
     if (rc)
         return print_error('B');
-
     rc = string_hello('C', &x3, &y3);
     if (rc)
         return print_error('C');
-
     if (!(if_tri(x2 - x1, y2 - y1, x1 - x3, y1 - y3)))
     {
         printf("Input coordinates(x, y) point O: ");
@@ -52,8 +49,8 @@ int main(void)
         printf("Error triagle!");
         return 1;
     }
-
-    switch (flag) {
+    switch (flag)
+    {
     case 2:
         printf("0");
         break;
@@ -63,7 +60,6 @@ int main(void)
     default:
         printf("2");
     }
-
     return 0;
 }
 
@@ -74,7 +70,6 @@ int if_tri(float x_1, float y_1, float x_2, float y_2)
     else
         return 0;
 }
-
 int find_point(float x_1, float y_1, float x_2, float y_2)
 {
     if ((x_1*y_2 - x_2*y_1) > 0)
@@ -84,7 +79,6 @@ int find_point(float x_1, float y_1, float x_2, float y_2)
     else
         return 0;
 }
-
 int give_information(float x1, float y1, float x2, float y2,
                       float x3, float y3, float xp, float yp)
 {
@@ -92,7 +86,6 @@ int give_information(float x1, float y1, float x2, float y2,
     s1 = find_point(x2 - x1, y2 - y1, xp - x1, yp - y1);
     s2 = find_point(x3 - x2, y3 - y2, xp - x2, yp - y2);
     s3 = find_point(x1 - x3, y1 - y3, xp - x3, yp - y3);
-
     if ((s1 < 0 && s2 < 0 && s3 < 0) ||
             (s1 > 0 && s2 > 0 && s3 > 0))
         flag = 2;
