7470edb0352429394da481467400f97bcc6db9f8
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-02 09:04:40 +0300

    Update main.c

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -3,19 +3,14 @@
 
 int main()
 {
-    float x1, y1, x2, y2, x3, y3, ab, ac, bc, p, xp, yp, sab, sac, sbc, r;
-    float s = 0, sum_all;
+    float x1, y1, x2, y2, x3, y3 , xp, yp, r, p, ab, ac, bc, s;
     printf("Enter coordinate of point's and then coordinates of triangle: ");
     r = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3, &xp, &yp);
-    ab = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    ac = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
-    bc = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+    ab = sqrt(pow(x2-x1,2) + pow(y2 - y1,2));
+    ac = sqrt(pow(x3-x1,2) + pow(y3 - y1,2));
+    bc = sqrt(pow(x3 - x2,2) + pow(y3 - y2,2));
     p = (ab + ac + bc) / 2;
     s = sqrt(p * (p - ab) * (p - ac) * (p - bc));
-    sab = (x1 * (y2 - yp) + x2 * (yp - y1) + xp * (y1 - y2)) / 2.0;
-    sbc = (xp * (y2 - y3) + x2 * (y3 - yp) + x3 * (yp - y2)) / 2.0;
-    sac = (x1 * (yp - y3) + xp * (y3 - y1) + x3 * (y1 - yp)) / 2.0;
-    sum_all = fabsf((fabsf(sab) + fabsf(sbc) + fabsf(sac)) - s);
     if (s == 0)
     {
         printf("not triangle");
@@ -29,17 +24,13 @@ int main()
             {
                 printf("1");
             }
-            else if(((xp < x1 && yp == y1) || (xp == x1 && yp < y1)) || ((xp < x2 && yp == y2) || (xp == x2 && yp < y2)) || ((xp < x3 && yp == y3) || (xp == x3 && yp < y3)))
-            {
-                printf("1");
-            }
-            else if (sum_all < (1e-1))
-            {
-                printf("0");
-            }
             else
             {
-                printf("2");
+                printf (
+                (((xp - x1) * (y2 - y1) - (yp - y1) * (x2 - x1)) * ((x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)) >= 0) &&
+                (((xp - x2)*(y3-y2)-(yp-y2)*(x3-x2))*((x1 - x2)*(y3-y2)-(y1-y2)*(x3-x2)) >= 0) &&
+                (((xp - x3)*(y1-y3)-(yp-y3)*(x1-x3))*((x2 - x3)*(y1-y3)-(y2-y3)*(x1-x3)) >= 0 )? 
+                "0": "2");
             }
         }
         else
@@ -47,5 +38,5 @@ int main()
             printf("input error");
             return 3;
         }
-    }   
+    } 
 }
