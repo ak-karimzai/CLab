d96855317c6b2ebcc449c0315d47e6e5a8db83d4
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-02 09:38:51 +0300

    Update main.c

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    float x1, y1, x2, y2, x3, y3 , xp, yp, r, p, ab, ac, bc, ab1, ac1, bc1, s;
+    float x1, y1, x2, y2, x3, y3, xp, yp, r;
     printf("Enter coordinate of point's and then coordinates of triangle: ");
     r = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3, &xp, &yp);
     if (r == 8)
@@ -16,8 +16,8 @@ int main()
         {    
             printf (
                 (((xp - x1) * (y2 - y1) - (yp - y1) * (x2 - x1)) * ((x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)) >= 0) &&
-                (((xp - x2)*(y3-y2)-(yp-y2)*(x3-x2))*((x1 - x2)*(y3-y2)-(y1-y2)*(x3-x2)) >= 0) &&
-                (((xp - x3)*(y1-y3)-(yp-y3)*(x1-x3))*((x2 - x3)*(y1-y3)-(y2-y3)*(x1-x3)) >= 0 )? 
+                (((xp - x2)*(y3 - y2) - (yp - y2) * (x3 - x2)) * ((x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)) >= 0) &&
+                (((xp - x3) * (y1 - y3) - (yp - y3) * (x1 - x3)) * ((x2 - x3) * (y1 - y3) - (y2 - y3) * (x1 - x3)) >= 0 )? 
                 "0": "2");
         }
     }
