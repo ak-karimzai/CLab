f93cb03368219b52d4b06b076cc123f1c4a40836
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-02 10:31:34 +0300

    Update main.c

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -3,33 +3,29 @@
 
 int main()
 {
-    float x1, y1, x2, y2, x3, y3, xp, yp, r, ab, ac, bc, s, p;
+    float x1, y1, x2, y2, x3, y3, xp, yp, r, a, b, c, s;
     printf("Enter coordinate of point's and then coordinates of triangle: ");
     r = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3, &xp, &yp);
-    ab = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    ac = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
-    bc = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-    p = (ab + ac + bc) / 2;
-    s = sqrt(p * (p - ab) * (p - ac) * (p - bc));
-    if (s == 0)
+    a = (x1 - xp) * (y2 - y1) - (x2 - x1) * (y1 - yp);
+    b = (x2 - xp) * (y3 - y2) - (x3 - x2) * (y2 - yp);
+    c = (x3 - xp) * (y1 - y3) - (x1 - x3) * (y3 - yp);
+    s = (x1 * y2 + x2 * y3 + x3 * y1) / 2 - (x2 * y1 + x3 * y2 + x1 * y3);
+    if (s <= 0)
     {
         printf("not triangle");
-        return 4;
+        return 3;
     }
     if (r == 8)
     {
-        if ((xp == x1 && yp == y1) || (xp == x2 && yp == y2) || (xp == x3 && yp == y3))
+        if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
         {
-            printf("1");
+            if(a==0 || b==0 || c==0)
+                printf("0");
+            else
+                printf("1");
         }
         else
-        {    
-            printf (
-                (((xp - x1) * (y2 - y1) - (yp - y1) * (x2 - x1)) * ((x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)) >= 0) &&
-                (((xp - x2) * (y3 - y2) - (yp - y2) * (x3 - x2)) * ((x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)) >= 0) &&
-                (((xp - x3) * (y1 - y3) - (yp - y3) * (x1 - x3)) * ((x2 - x3) * (y1 - y3) - (y2 - y3) * (x1 - x3)) >= 0 )? 
-                "0": "2");
-        }
+            printf("2");
     }
     else
     {
