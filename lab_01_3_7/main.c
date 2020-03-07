50300cda19049064c61447cf6723eb551029f641
Karimzai Ahmad Khalid <857-ka19iu10@users.noreply.git.iu7.bmstu.ru>
2020-03-07 15:11:36 +0300

    Update main.c

--- lab_01_3_7/main.c
+++ lab_01_3_7/main.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
 #include<math.h>
 
-float estimate(float x, float eps)
+double estimate(double x, double eps)
 {
-    float s_part = x;
-    float s_sum = 0;
-    int n = 3;
-
-    while (fabsf(s_part) > eps)
-    {
-        s_sum += s_part;
-        s_part *= (-1 * x * x) / n;
-        n += 2;
-    }
-    return s_sum;
+	long long i = 1;
+	double t = x;
+	double res = t;
+	while (fabs(t) > eps)
+	{
+		i += 2;
+		t *= (2 - i) * x * x / i;
+		res += t;
+	}
+	return res;
 }
 int main(void)
 {
-    float sum_s, f_x, eps, abs_err, rel_err;
-    float x;
+    double sum_s, f_x, eps, abs_err, rel_err;
+    double x;
     int r;
 
     printf("enter x and eps\n");
-    r = scanf("%f%f", &x, &eps);
+    r = scanf("%lf%lf", &x, &eps);
     if (r == 2)
     {
         if (eps >= 1 || eps <= 0)
@@ -30,7 +29,7 @@ int main(void)
             printf("epsilon error");
             return 2;
         }
-        else if (fabsf(x) > 1)
+        else if (fabs(x) > 1)
         {
             printf("vlaue error");
             return 3;
@@ -38,10 +37,10 @@ int main(void)
         else
         {
             sum_s = estimate(x, eps);
-            f_x = atanf(x);
-            abs_err = fabsf(f_x - sum_s);
-            rel_err = fabsf((f_x - sum_s) / f_x);
-            printf("%f %f %f %f", sum_s, f_x, abs_err, rel_err);
+            f_x = atan(x);
+            abs_err = fabs(f_x - sum_s);
+            rel_err = fabs((f_x - sum_s) / f_x);
+            printf("%lf %lf %lf %lf", sum_s, f_x, abs_err, rel_err);
 
             return 0;
         }
@@ -52,4 +51,3 @@ int main(void)
         return 1;
     }
 }
-
