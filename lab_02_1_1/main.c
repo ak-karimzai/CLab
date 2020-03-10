d0e05a735703d1cd5be427fe88c1fee049b7482c
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-03-10 11:13:22 +0300

    added lab_2_1

---
+++ lab_02_1_1/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#define N 100
+
+int append(int A[], int i, int n)
+{
+    if (n)
+    {
+        A[i] = n;
+    }
+    else
+    {
+        return -1;
+    }
+}
+int main()
+{
+    int A[N] = {0}, geo_mean = 1.0, x;
+    int i, n;
+    printf("Enter the size of array.\n");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        append(A, i, x);
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (A[i] % 2 != 0)
+        {
+            geo_mean *= A[i];
+        }
+    }
+    printf("geometrical mean is equal: \n");
+    printf("%d", geo_mean);
+    return 0;
+}
