49c3c814951d6d2c52e786d27b5f746f397ac732
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-03-10 12:40:50 +0300

    Renewed lab

--- lab_02_1_1/main.c
+++ lab_02_1_1/main.c
@@ -1,41 +1,98 @@
 #include <stdio.h>
 #include <math.h>
-#define N 100
+#define N 10
+#define size_error -1
+#define element_error -2
+#define array_nfull -3
+#define incorect_sinput -4
+#define input_error -5
+#define log_error -6
 
-int append(int a[], int i, int n)
+int read_array(int *const a, int *n)
 {
-    if (n)
+    int rc, array_i;
+    int count = 0;
+    rc = scanf("%d", n);
+    if(rc == 1)
     {
-        a[i] = n;
-	return 0;
-    }
-    return -1;
-}
-int main()
-{
-    int a[N] = {0}, geo_mean = 1.0, x;
-    int i, n, rc = 0;
-    printf("Enter the size of array.\n");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        rc = scanf("%d", &x);
-        if (rc)
-            append(a, i, x);
+        if (*n > N || * n < 1)
+        {
+            printf("invlaid Input for size");
+            return size_error;
+        }
         else
         {
-            printf("inPut Error!\n");
-            return -1;
+            for (int i = 0; i < *n; i++)
+            {
+                rc = scanf("%d", &array_i);
+                if (rc == 1)
+                {
+                    a[i] = array_i;
+                    count++;
+                }
+                else
+                {
+                    printf("Invalid element input")
+                    return element_error;
+                }
+            }
+            if (count != *n)
+            {
+                printf("Array not full");
+                return array_nfull;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
-    for (i = 0; i < n; i++)
+    else
+    {
+        printf("incorrect input Size");
+        return incorrect_sinput;
+    }
+}
+int geo_averge(const int *const a, const int n)
+{
+    float geo_av = 1;
+    int even_counter = 0;
+    for (int i = 0; i < n; i++)
     {
         if (a[i] % 2 != 0)
         {
-            geo_mean *= a[i];
+            even_counter++;
+            geo_av *= a[i];
         }
     }
-    printf("geometrical mean is equal: \n");
-    printf("%d", geo_mean);
-    return 0;
+    if(even_counter == 0)
+    {
+        printf("Array without event numbers");
+        geo_av = 0;
+        return log_error;
+    }
+    return geo_av;
+}
+int main()
+{
+    int a[N] = {0}, n = 0;
+
+    if (read_array(a, &n))
+    {
+        return input_error;
+    }
+    else
+    {
+        const float geo_avr = geo_averge(a, n);
+
+        if (geo_avr == log_error)
+        {
+            return log_error;
+        }
+        else
+        {
+            printf("%.4f", geo_avr);
+            return 0;
+        }
+    }
 }
