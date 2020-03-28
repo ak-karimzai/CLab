5f86e8c3ab00a1ce8aca7f10324d802d090a4b6d
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-03-28 11:00:25 +0300

    1st ecercise

--- lab_03_3_1/main.c
+++ lab_03_3_1/main.c
@@ -9,88 +9,91 @@
 
 #define N 10
 
-int read_matrix(int a[N][N], int *const n, int *const m)
+int read_mat(int a[N][N], int *const m, int *const n)
 {
-    int rc, matrix_el;
+    int rc, matrix_i;
     int count = 0;
-
+    printf("Enter Dimensions:\n");
     rc = scanf("%d%d", m, n);
     if (rc == 2)
     {
         if (*n > N || *n < 1 || *m > N || *m < 1)
         {
-            printf("size input error");
+            printf("size input error!\n");
             return SIZE_I_ERROR;
         }
         else
         {
-            for (int i = 0; i < *m; ++i)
+            for (int i = 0; i < *m; i++)
             {
                 for (int j = 0; j < *n; j++)
                 {
-                    rc = scanf("%d", &matrix_el);
+                    rc = scanf("%d", &matrix_i);
                     if (rc)
                     {
-                        a[i][j] = matrix_el;
+                        a[i][j] = matrix_i;
                         count++;
                     }
                     else
                     {
-                        printf("element input error");
+                        printf("incorrect element input!\n");
                         return ELEM_I_ERROR;
                     }
                 }
             }
-
-            if (count != (*n) * (*m))
+            if (count != (*m) * (*n))
             {
-                printf("arguments count error");
+                printf("argument's vount error\n");
                 return ARG_C_ERROR;
             }
             else
+            {
                 return FINISHED;
+            }
         }
     }
     else
     {
-        printf("Input clearance error");
-        return CLEAR_I_ERROR;
+        printf("invalid size input\n");
+        return INPUT_ERROR;
     }
 }
-
-void display(int *const a, const int n)
+void display(int a[N][N], int m, int n)
 {
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < m;i++)
     {
-        printf("%d ", a[i]);
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
     }
 }
-
-int is_monotone(int a[N][N], int *const b, const int n, const int m)
+int is_monotone(int a[N][N], int *const b, int m, int n)
 {
     int is_ordered_up, is_ordered_down;
 
     if (m == 1)
     {
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < m; ++i)
             b[i] = 0;
     }
     else
     {
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < m; ++i)
         {
             is_ordered_up = 1;
             is_ordered_down = 1;
 
-            for (int j = 0; j < m - 1; ++j)
+            for (int j = 0; j < n - 1; ++j)
                 if (a[i][j] <= a[i][j + 1])
                     is_ordered_up++;
 
-            for (int j = 0; j < m - 1; ++j)
+            for (int j = 0; j < n - 1; ++j)
                 if (a[i][j] >= a[i][j + 1])
                     is_ordered_down++;
 
-            if (is_ordered_up == m || is_ordered_down == m)
+            if (is_ordered_up == n || is_ordered_down == n)
                 b[i] = 1;
             else
                 b[i] = 0;
@@ -98,18 +101,23 @@ int is_monotone(int a[N][N], int *const b, const int n, const int m)
     }
     return FINISHED;
 }
-
+void display_1(int *const a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
 int main()
 {
     int a[N][N], m, n;
     int b[N];
-
-    if (read_matrix(a, &m, &n))
+    if (read_mat(a, &m, &n))
         return INPUT_ERROR;
     else
     {
         is_monotone(a, b, m, n);
-        display(b, m);
+        display_1(b, m);
         return FINISHED;
     }
-}
+} 
