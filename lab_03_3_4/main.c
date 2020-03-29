dd617a27c2e292aa19f4caf4b832b5a066eeb8ff
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-03-29 11:43:38 +0300

    4rth task

---
+++ lab_03_3_4/main.c
@@ -0,0 +1,137 @@
+ 
+
+#include <stdio.h>
+
+#define OK 0
+#define SIZE_INPUT_ERR -1
+#define CLEAR_INPUT_ERR -2
+#define ELEMENT_INPUT_ERR -3
+#define ARG_COUNT_ERR -4
+#define INPUT_ERR -5
+
+#define ORDERED 1
+#define NOT_ORDERED 0
+
+#define N 10
+
+struct matrix 
+{
+    int a[N][N];
+    int n;
+};
+
+int read_matrix(struct matrix *mtr)
+{
+    int rc, matrix_el;
+    int arg_count = 0;
+
+    printf("Enter dimension:\n");
+    rc = scanf("%d", &mtr->n);
+
+    if (rc == 1)
+    {
+        if (mtr->n > N || mtr->n < 1)
+        {
+            printf("Matrix size input error");
+
+            return SIZE_INPUT_ERR;
+        }
+        else
+        {
+            for (int i = 0; i < mtr->n; ++i)
+            {
+                for (int j = 0; j < mtr->n; j++)
+                {
+                    rc = scanf("%d", &matrix_el);
+                    if (rc == 1)
+                    {
+                        mtr->a[i][j] = matrix_el;
+                        arg_count++;
+                    }
+                    else
+                    {
+                        printf("Array element input error");
+
+                        return ELEMENT_INPUT_ERR;
+                    }
+                }
+            }
+
+            if (arg_count != (mtr->n) * (mtr->n))
+            {
+                printf("Arguments' count error");
+
+                return ARG_COUNT_ERR;
+            }
+            else
+                return OK;
+        }
+    }
+    else
+    {
+        printf("Input clearance error");
+
+        return CLEAR_INPUT_ERR;
+    }
+}
+
+void transp(struct matrix *mtr)
+{
+    struct matrix matr;
+    matr.n = mtr->n;
+    for (int i = 0; i < mtr->n; i++)
+    {
+        for (int j = 0; j < mtr->n; j++)
+        {
+            matr.a[i][j] = mtr->a[j][i];
+        }
+    }
+    *mtr = matr;
+}
+
+void swap_matr(struct matrix *mtr)
+{
+    int m = 0, mn = mtr->n - 1;
+    int temp[N], start = 0, stop = mtr->n;
+    while (m != mn)
+    {
+        for (int i = start; i < stop; i++)
+        {
+            temp[i] = mtr->a[m][i];
+            mtr->a[m][i] = mtr->a[mn][i];
+            mtr->a[mn][i] = temp[i];
+        }
+        m++;
+        mn--;
+        start++;
+        stop--;
+    }
+}
+
+void display(struct matrix mtr)
+{
+    for (int i = 0; i < mtr.n; i++)
+    {
+        for (int j = 0; j < mtr.n; j++)
+        {
+            printf("%d ", mtr.a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    struct matrix mtr;
+
+    if (read_matrix(&mtr))
+        return INPUT_ERR;
+    else
+    {
+        transp(&mtr);
+        swap_matr(&mtr);
+        transp(&mtr);
+        display(mtr);
+        return OK;
+    }
+}
