4d43955df1eb2f99d092d3cb3d82daeeb9082c48
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-03-29 07:51:23 +0300

    2nd task

--- lab_03_2_2/main.c
+++ lab_03_2_2/main.c
@@ -12,17 +12,24 @@
 
 #define N 10
 
-int read_matrix(int a[N][N], int *const n, int *const m)
+struct matrix
+{
+    int a[N][N];
+    int m;
+    int n;
+};
+
+int read_matrix(struct matrix *matr)
 {
     int rc, matrix_el;
     int arg_count = 0;
 
     printf("Enter dimensions':\n");
-    rc = scanf("%d%d", n, m);
+    rc = scanf("%d%d", &matr->m, &matr->n);
 
     if (rc == 2)
     {
-        if (*n > N || *n < 1 || *m > N || *m < 1)
+        if (matr->m > N || matr->m < 1 || matr->n > N || matr->n < 1)
         {
             printf("Matrix size input error");
 
@@ -30,14 +37,14 @@ int read_matrix(int a[N][N], int *const n, int *const m)
         }
         else
         {
-            for (int i = 0; i < *n; ++i)
+            for (int i = 0; i < matr->m; ++i)
             {
-                for (int j = 0; j < *m; j++)
+                for (int j = 0; j < matr->n; j++)
                 {
                     rc = scanf("%d", &matrix_el);
                     if (rc == 1)
                     {
-                        a[i][j] = matrix_el;
+                        matr->a[i][j] = matrix_el;
                         arg_count++;
                     }
                     else
@@ -49,7 +56,7 @@ int read_matrix(int a[N][N], int *const n, int *const m)
                 }
             }
 
-            if (arg_count != (*n) * (*m))
+            if (arg_count != matr->m * matr->n)
             {
                 printf("Arguments' count error");
 
@@ -67,17 +74,39 @@ int read_matrix(int a[N][N], int *const n, int *const m)
     }
 }
 
-int if_even(int a[N][N], int m, int n)
+void append_1(struct matrix *mat, int k)
+{
+    for (int i = mat->m; i > k; i--)
+    {
+        for (int j = 0; j < mat->n; j++)
+            mat->a[i][j] = mat->a[i - 1][j];
+    }
+    for (int i = 0; i < mat->m; i++)
+    {
+        for (int j = 0; j < mat->n; j++)
+        {
+            if (i == k)
+            {
+                mat->a[i][j] = -1;
+            }
+        }
+    }
+    mat->m++;
+}
+
+
+int if_even(struct matrix *matr)
 {
     int flag;
-    int sum, elem;
-    for (int i = 0; i < m; i++)
+    int sum, elem, k = 0;
+    static int b[N];
+    for (int i = 0; i < matr->m; i++)
     {
         flag = 0;
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < matr->n; j++)
         {
             sum = 0;
-            elem = a[i][j];
+            elem = matr->a[i][j];
             while (elem)
             {
                 sum = elem % 10;
@@ -88,22 +117,20 @@ int if_even(int a[N][N], int m, int n)
         }
         if (flag >= 2)
         {
-            for (int j = 0; j < n; j++)
-            {
-                a[i][j] = -1;
-            }
+            append_1(matr, i);
+            i++;
         }
     }
-    return OK;
+    return b;
 }
 
-void display(int a[N][N], int m, int n)
+void display(struct matrix matr)
 {
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < matr.m; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < matr.n; j++)
         {
-            printf("%d ", a[i][j]);
+            printf("%d ", matr.a[i][j]);
         }
         printf("\n");
     }
@@ -111,14 +138,14 @@ void display(int a[N][N], int m, int n)
 
 int main()
 {
-    int a[N][N], m, n;
-
-    if (read_matrix(a, &m, &n))
+    struct matrix matr;
+    int *p;
+    if (read_matrix(&matr))
         return INPUT_ERR;
     else
     {
-        if_even(a, m, n);
-        display(a, m, n);
+        p = if_even(&matr);
+        display(matr);
         return OK;
     }
 }
