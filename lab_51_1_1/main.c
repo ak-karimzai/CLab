3106c73cd89b19180f0117a508cb21821ff830dc
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-05-11 14:36:40 +0300

    first task

---
+++ lab_51_1_1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void find_two_max(int max)
+{
+    int max_1, max_2;
+    while (fscanf(stdin, "%d", &max_1) == 1)
+    {
+        max_2 = max;
+        if (max_1 > max)
+            max = max_1;
+    }
+    printf("%d %d", max_1, max_2);
+}
+int main()
+{
+    int max;
+    if (fscanf(stdin, "%d", &max) == 0 || feof(stdin))
+    {
+        printf("I/O Error\n");
+        return -1;
+    }
+    find_two_max(max);
+    return 0;
+}
