deb3f52a90de604a148d889f3de1534a30efd0b5
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-05-12 15:11:39 +0300

    first task fifth lab

--- lab_51_1_1/main.c
+++ lab_51_1_1/main.c
@@ -1,29 +1,5 @@
 #include <stdio.h>
-
-void swap(int *x, int *y)
-{
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void find_two_max(int max)
-{
-    int max_1, max_2;
-
-    fscanf(stdin, "%d", &max_1);
-    if (max_1 > max)
-        swap(&max, &max_1);
-    
-    while (fscanf(stdin, "%d", &max_2) == 1)
-    {
-        if (max_2 > max_1)
-            swap(&max_1, &max_2);
-        if (max_1 > max)
-            swap(&max_1, &max);
-    }
-    printf("%d %d", max, max_1);
-}
+#include "function.h"
 
 int main()
 {
@@ -31,8 +7,8 @@ int main()
     if (fscanf(stdin, "%d", &max) == 0 || feof(stdin))
     {
         printf("I/O Error\n");
-        return -1;
+        return error;
     }
     find_two_max(max);
-    return 0;
+    return ok;
 }
