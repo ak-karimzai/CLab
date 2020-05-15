7d062e14ec00b766b1db40f8524b496a02182dc5
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-05-15 11:32:03 +0300

    3rd Task 5th lab

--- lab_51_3/main.c
+++ lab_51_3/main.c
@@ -38,44 +38,60 @@ void sort_arr(arr *a)
     }
 }
 
-int main()
+int main(int argc, char **argv)
 {
     arr in_arr, out_arr;
     FILE *in, *out;
     
-    in = fopen("p_file.bin", "wb");
+    in = fopen(argv[1], "wb");
+    
     if (in == NULL)
     {
         return 1;
     }
+    
     add_to_array(&in_arr);
+    
     size_t element = fwrite(&in_arr, sizeof(arr), 1, in);
+    
     fclose(in);
+    
     if (element == 0)
     {
         return 2;
     }
-    out = fopen("p_file.bin", "rb");
+    
+    out = fopen(argv[1], "rb");
 
     element = fread(&out_arr, sizeof(arr), 1, out);
+    
     fclose(out);
+    
     if (element == 0)
     {
         return 3;
     }
+    
     for (int i = 0; i < out_arr.n; i++)
     {
         printf("%d ", out_arr.a[i]);
     }
+    
     sort_arr(&out_arr);
-    in = fopen("p_file.bin", "wb");
+
+    in = fopen(argv[2], "wb");
+    
     if (in == NULL)
         return 1;
+    
     element = fwrite(&out_arr, sizeof(arr), 1, in);
+    
     fclose(in);
+    
     if (element == 0)
     {
         return 3;
     }
+
     return 0;
 }
