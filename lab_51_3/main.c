fa35324a2e00f7bfffab3b1145c5081659cafdfa
Ahmad khalid <Ak.karimzai1@mail.ru>
2020-05-22 09:51:43 +0300

    task 3

--- lab_51_3/main.c
+++ lab_51_3/main.c
@@ -4,6 +4,17 @@
 
 #define N 100
 
+enum type_error
+{
+    ok,
+    arg_err,
+    file_err,
+    empty_file,
+    put_err,
+    get_err,
+    error
+};
+
 void add_to_array(FILE *f, int n)
 {
     fseek(f, 0, SEEK_SET);
@@ -28,16 +39,16 @@ int get_by_pos(FILE *f, int pos, int *x)
 {
     fseek(f, pos * sizeof(int) - sizeof(int), SEEK_SET);
     if (fread(x, sizeof(int), 1, f) == 0)
-        return -1;
-    return 0;
+        return get_err;
+    return ok;
 }
 
 int put_by_pos(FILE *f, int pos, int x)
 {
     fseek(f, pos * sizeof(int) - sizeof(int), SEEK_SET);
     if (fwrite(&x, sizeof(int), 1, f) == 0)
-        return -1;
-    return 0;
+        return put_err;
+    return ok;
 }
 
 void swap(int *x, int *y)
@@ -66,7 +77,7 @@ int sort_arr(FILE *f, int n)
     for (int i = 0; i < n; i++)
     {
         if (get_by_pos(f, i + 1, &x))
-            return -1;
+            return get_err;
 
         arr[i] = x; 
     }
@@ -74,9 +85,9 @@ int sort_arr(FILE *f, int n)
     for (int i = 0; i < n; i++)
     {
         if (put_by_pos(f, i + 1, arr[i]))
-            return -1;
+            return put_err;
     }
-    return 0;
+    return ok;
 }
 
 void display(FILE *f, int n)
@@ -96,23 +107,23 @@ int main(int argc, char **argv)
     FILE *in;
 
     if (argc < 3) 
-        return -1;
+        return arg_err;
     
-    if (strcmp(argv[1], "c") == 0)
+    if (strcmp(argv[1], "c") == ok)
     {
         int n = atoi(argv[2]);
         if (n == 0)
-            return -1;
+            return get_err;
         
         in = fopen(argv[3], "wb");
         
         if (in == NULL)
-            return -1;
+            return file_err;
 
         add_to_array(in, n);
 
         fclose(in);
-        return 0;
+        return ok;
     }
 
     if (strcmp(argv[1], "p") == 0)
@@ -120,18 +131,18 @@ int main(int argc, char **argv)
         in = fopen(argv[2], "rb");
         
         if (in == NULL)
-            return -1;
+            return file_err;
 
         int n = file_size(in);
         
         if (n == 0)
-            return -1;
+            return empty_file;
 
         display(in, n);
 
         fclose(in);
         
-        return 0;
+        return ok;
     }
     
     if (strcmp(argv[1], "s") == 0)
@@ -141,11 +152,11 @@ int main(int argc, char **argv)
         int n = file_size(in);
 
         if (n == 0)
-            return -1;
+            return empty_file;
         
         if (sort_arr(in, n))
-            return -1;
+            return put_err;
 
-        return 0;
+        return ok;
     }
 }
