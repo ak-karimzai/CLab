009b3a1fb83e7f217f69c7854c0e3be31a91fdbd
Ahmad Khalid Karimzai <ka19iu10@git.iu7.bmstu.ru>
2020-09-09 19:48:22 +0300

    check for test

--- lab_06_03_01/main.c
+++ lab_06_03_01/main.c
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
         }
     }
     else
-        return read_err;
+        return file_err;
 
     if (read_from_file(input_file, pro, &num_of_products)) 
     {
