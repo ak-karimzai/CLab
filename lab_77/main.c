923ebe0ea62831341785c88a4033b6ec7668bfaf
U-VLAB\user-lab01 <user-lab01@win10-lab1-35.vlab.bmstu.ru>
2020-09-24 10:03:50 +0300

    functions

--- lab_77/main.c
+++ lab_77/main.c
@@ -3,22 +3,96 @@
 #include <string.h>
 #include "sort.h"
 
+#define INPUT_FILE 1
+#define OUTPUT_FILE 2
+
 enum error_type
 {
     ok,
     error
 };
 
-int main()
+int file_count_objs(FILE *file)
 {
-    int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    // char arr_char[] = {'z', 'y', 'x', 'w', 'v', 'u', 'p'};
-    mysort(arr, arr + 10, sizeof(arr[0]), compare_int);
-    // mysort(arr_char, arr_char + 7, sizeof(char), compare_char);
-    for (int i = 0; i < 10; i++)
-        fprintf(stdout, "%d ", arr[i]);
-    puts("\nfinished");
-    // for (int i = 0; i < 7; i++)
-    //     fprintf(stdout, "%c ", arr_char[i]);
+    int count = 0, temp;
+    
+    while (fscanf(file, "%d", &temp) == 1)
+        count++;
+    
+    if (feof(file) || temp == EOF)
+    {
+        rewind(file);
+        return count;
+    }
     return 0;
+}
+
+int *read_objs_from_file(FILE *file, const int num_of_objs)
+{
+    int rc = ok;
+    int *arr = malloc(num_of_objs * sizeof(int));
+    if (arr)
+    {
+        for (size_t i = 0; i < num_of_objs; i++)
+        {
+            if (fscanf(file, "%d", (arr + i)) != 1)
+            {
+                rc = error;
+            }
+        }
+    }
+    else
+        rc = error;
+    return rc == ok ? arr : NULL;
+}
+
+void write_objs_in_file(FILE *file, const int *arr, const int num_of_objs)
+{
+    for (int i = 0; i < num_of_objs; i++)
+        fprintf(file, "%d ", *(arr + i));
+}
+
+int main(int argc, char **argv)
+{
+    int rc = error;
+    FILE *input_file, *output_file;
+    int *arr;
+    int num_of_objs;
+    if (argc == 3)
+    {
+        puts(argv[INPUT_FILE]);
+        input_file = fopen(argv[INPUT_FILE], "r");
+        output_file = fopen(argv[OUTPUT_FILE], "w");
+        if (!input_file || !output_file)
+        {
+            puts("error");
+            rc = error;
+        }
+        else
+        {
+            num_of_objs = file_count_objs(input_file);
+            if (num_of_objs)
+            {
+                arr = read_objs_from_file(input_file, num_of_objs);
+                if (arr)
+                {
+                    mysort(arr, arr + num_of_objs, sizeof(arr[0]), compare_int);
+                    write_objs_in_file(output_file, arr, num_of_objs);
+                }
+                else
+                    rc = error;
+            }
+            else
+                rc = error;
+        }
+        fclose(output_file);
+        fclose(input_file);
+        free(arr);
+    }
+    else
+    {
+        rc = error;
+    }
+    
+    return rc;
 }
