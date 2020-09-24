cc3bcb3bb030cb4231d92aab2abfeedf310ecc6f
Ahmad Khalid Karimzai <ka19iu10@git.iu7.bmstu.ru>
2020-09-24 23:49:21 +0300

    lab_07 files

--- lab_77/function.h
+++
@@ -1,4 +0,0 @@
-int file_count_objs(FILE *file);
-int *read_objs_from_file(FILE *file, const int num_of_objs);
-int key(const int *pb_src, const int *pe_src, int **pb_dst, int **pe_dst);
-void write_objs_in_file(FILE *file, const int *arr_lhs, const int *arr_rhs);
--- lab_77/sort.h
+++
@@ -1,4 +0,0 @@
-typedef int (*compare)(void *lhs, void *rhs);
-
-void mysort(void *arr_lhs, void *arr_rhs, const size_t size, compare comp);
-int compare_int(void *lhs, void *rhs);
