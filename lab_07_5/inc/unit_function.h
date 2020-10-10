36272417a86d5758f8667de6beaad2bb36e5ce86
not found <Ak.karimzai>
2020-10-10 17:26:15 +0300

    check the unit testsma

--- lab_07_5/inc/unit_function.h
+++ lab_07_5/inc/unit_function.h
@@ -1,7 +1,7 @@
 #ifndef UNIT_FUNCTION_H
 #define UNIT_FUNCTION_H
 
-int key_tests_final_result(void);
-int calc_sort_test();
+int key_unit_tests(void);
+// int calc_sort_test();
 
 #endif // UNIT_FUNCTION_H
