3c9ea088875b04d59e65e71c902d5bd279837a7b
not found <Ak.karimzai>
2020-10-15 22:52:34 +0300

    main function updated

--- lab_07_5/main.c
+++ lab_07_5/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv)
 {
-    int rc = ok;
+    int rc = error;
     FILE *input_file, *output_file;
     int *arr = NULL;
     int num_of_objs = 0;
@@ -13,16 +13,13 @@ int main(int argc, char **argv)
         input_file = fopen(argv[INPUT_FILE], "r");
         output_file = fopen(argv[OUTPUT_FILE], "w");
         if (input_file == NULL || output_file == NULL)
-            rc = error;
-        else
         {
             arr = procees_data_from_file(input_file, &num_of_objs);
-            if (!arr)
-                rc = error;
-            else
+            if (arr)
             {
                 mysort(arr, num_of_objs, sizeof(arr[0]), compare_int);
                 write_objs_in_file(output_file, arr, arr + num_of_objs);
+                rc = ok;
             }
             fclose(output_file);
             fclose(input_file);
@@ -31,27 +28,20 @@ int main(int argc, char **argv)
     }
     else if (argc == 4 && strcmp(argv[KEY], "f") == ok)
     {
-        // puts("i'm here");
         input_file = fopen(argv[INPUT_FILE], "r");
         output_file = fopen(argv[OUTPUT_FILE], "w");
-        if (input_file == NULL || output_file == NULL)
-            rc = error;
-        else
+        if (input_file != NULL || output_file != NULL)
         {
             arr = procees_data_from_file(input_file, &num_of_objs);
-            if (!arr)
-                rc = error;
-            else
+            if (arr)
             {
                 int *arr_lhs = NULL, *arr_rhs = NULL;
-                if (key(arr, arr + num_of_objs, &arr_lhs, &arr_rhs) != ok)
-                    rc = error;
-                else
+                if (key(arr, arr + num_of_objs, &arr_lhs, &arr_rhs) == ok)
                 {
                     mysort(arr_lhs, (arr_rhs - arr_lhs), sizeof(arr_rhs[0]), compare_int);
                     write_objs_in_file(output_file, arr_lhs, arr_rhs);
-                    // printf("%d\n", *(arr_lhs +   1));
                     free(arr_lhs);
+                    rc = ok;
                 }
             }
 
@@ -60,8 +50,6 @@ int main(int argc, char **argv)
             free(arr);
         }
     }
-    else
-        rc = error;
     
     return rc;
 }
