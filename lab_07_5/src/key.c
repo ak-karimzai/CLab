0dcb331343edb6a984a79680fa9e0b5470d3caa5
not found <Ak.karimzai>
2020-10-24 14:42:08 +0300

    key function reviwed

--- lab_07_5/src/key.c
+++ lab_07_5/src/key.c
@@ -39,10 +39,7 @@ int key(const int *pb_src, const int *pe_src, int **pb_dst, int **pe_dst)
     int rc = ok;
     int count = 0, sum;
     if (!pb_src || pe_src - pb_src <= 0)
-    {
-        *pb_dst = *pe_dst = NULL;
         rc = error;
-    }
     else
     {
         count = get_num_of_elements(pb_src, pe_src, &sum);
@@ -52,9 +49,7 @@ int key(const int *pb_src, const int *pe_src, int **pb_dst, int **pe_dst)
         {
             *pb_dst = malloc(count * sizeof(int));
             if (*pb_dst == NULL)
-            {
                 rc = error;
-            }
             else
             {
                 add_elements_to_array(*pb_dst, pb_src, pe_src, sum);
