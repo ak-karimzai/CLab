54792243b2308bf338100a4db6ce70353d701fd6
not found <Ak.karimzai>
2020-10-31 22:50:26 +0300

    just for check

--- lab_09_3_1/src/io.c
+++ lab_09_3_1/src/io.c
@@ -6,37 +6,38 @@
 void free_product_arr(product *products, const int num_of_objs)
 {
     for (int i = 0; i < num_of_objs; i++)
-        free(products[i].product_name);
+        if (products[i].product_name)
+            free(products[i].product_name);
     free(products);
-}
-
-int num_of_objs(FILE *input_file)
-{
-    int x;
-    if (fscanf(input_file, "%d\n", &x) == 1 && x >= 0)
-        return x;
-    return 0;
-}
-
-void init_produnct_node(FILE *input_file, product *pro)
-{
-    size_t len;
-    getline(&pro->product_name, &len, input_file);
-    fscanf(input_file, "%d\n", &pro->price);
 }
 
 product *read_from_file(FILE *input_file, int *num_of_products)
 {
-    *num_of_products = num_of_objs(input_file);
+    int rc = ok;
     product *products = NULL;
-
-    if (*num_of_products)
+    if (fscanf(input_file, "%d\n", num_of_products) != 1 || *num_of_products <= 0)
+        rc = error;
+    else
     {
         products = malloc(*num_of_products * sizeof(product));
-        for (int i = 0; i < *num_of_products; i++)
-            init_produnct_node(input_file, &products[i]);
+        if (products == NULL)
+            rc = error;
+        else
+        {
+            for (int i = 0; i < *num_of_products; i++)
+            {
+                size_t len;
+                ssize_t read = getline(&products[i].product_name, &len, input_file);
+                if (read == -1 || fscanf(input_file, "%d\n", &products[i].price) != 1 || products[i].price <= 0)
+                {
+                    free_product_arr(products, *num_of_products);
+                    rc = error;
+                    break;
+                }
+            }
+        }
     }
-    return products;
+    return rc == ok ? products : NULL;
 }
 
 void display_to_screen(product *products, const int num_of_elem)
