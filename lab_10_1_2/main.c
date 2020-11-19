a11b470263549db54d4407e4705c5fd6ed2bb3cb
not found <Ak.karimzai>
2020-11-19 21:44:09 +0300

    just for check

--- lab_10_1_2/main.c
+++ lab_10_1_2/main.c
@@ -1,131 +1,323 @@
-#include "./inc/main.h"
+#include "main.h"
+
+void free_list(ll_polynome_t *polynome)
+{
+    if (polynome)
+    {
+        node_t *temp;
+        while (polynome->head)
+        {
+            temp = polynome->head;
+            polynome->head = polynome->head->next;
+            free(temp);
+        }
+        free(polynome);
+    }
+}
+
+char *get_string(void)
+{
+    char *line = NULL;
+    size_t len;
+    ssize_t read;
+    read = getline(&line, &len, stdin);
+    if (read > 0)
+        line[read - 1] = '\0';
+    return read > 0 ? line : NULL;
+}
+
+int myatoi(char *s, int *x)
+{
+    int rc = ok;
+    size_t len = strlen(s);
+    for (size_t i = 0; i < len; i++)
+        if (s[i] < '0' && s[i] > '9')
+            rc = error;
+    *x = rc == ok ? atoi(s) : 0;
+    return rc; 
+}
+
+node_t *create_node(int coeff, int pow)
+{
+    node_t *temp = malloc(sizeof(node_t));
+    if (temp)
+    {
+        temp->coeff = coeff;
+        temp->pow = pow;
+        temp->next = NULL;
+    }
+    return temp;
+}
+
+int push(ll_polynome_t *polynome, node_t *node)
+{
+    int rc = ok;
+    if (node)
+    {
+        if (polynome->head == NULL)
+            polynome->head = polynome->end = node;
+        else if (polynome->end->pow >= node->pow)
+        {
+            polynome->end->next = node;
+            polynome->end = node;
+        }
+        else
+        {
+            rc = error;
+            free(node);
+        }
+    }
+    else
+        rc = error;
+    return rc;
+}
+
+int create_list_from_line(char *str, ll_polynome_t *polynome)
+{
+    int rc = ok, coeff, pow;
+    char *num = strtok(str, " ");
+    if (num == NULL)
+        rc = error;
+    while (num)
+    {
+        if (myatoi(num, &coeff) == ok && (num = strtok(NULL, " ")) && myatoi(num, &pow) == ok)
+        {
+            if (push(polynome, create_node(coeff, pow)) == error)
+            {
+                rc = error;
+                break;
+            }
+        }
+        else
+        {
+            rc = error;
+            break;
+        }
+        num = strtok(NULL, " ");
+    }
+    if (rc == error)
+        free_list(polynome);
+    return rc;
+}
+
+int create_ll_node(ll_polynome_t **_restrict)
+{
+    int rc = error;
+    *_restrict = malloc(sizeof(ll_polynome_t));
+    if (*_restrict)
+    {
+        (*_restrict)->head = (*_restrict)->end = NULL;
+        rc = ok;
+    }
+    return rc;
+}
+
+long int polynome_val(ll_polynome_t *polynome, int value)
+{
+    node_t *temp = polynome->head;
+    long int res = 0;
+    while (temp)
+    {
+        res += ((long int) pow(value, temp->pow) * temp->coeff);
+        temp = temp->next;
+    }
+    return res;
+}
+
+void print_list(ll_polynome_t *polynome, int flag)
+{
+    node_t *temp = polynome->head;
+    if (flag == derivative)
+    {
+        while (temp)
+        {
+            if (temp->pow != -1)
+                printf("%d %d ", temp->coeff, temp->pow);
+            temp = temp->next;
+        }
+        printf("L\n");
+    }
+    else if (flag == just_print)
+    {
+        while (temp)
+        {
+            printf("%d %d ", temp->coeff, temp->pow);
+            temp = temp->next;
+        }
+        printf("L\n");
+    }
+    else if (flag == sum_print)
+    {
+        while (temp)
+        {
+            if (temp->coeff != 0)
+                printf("%d %d ", temp->coeff, temp->pow);
+            temp = temp->next;
+        }
+        printf("L\n");
+    }
+}
+
+void derivative_if_polynome(ll_polynome_t *polynome)
+{
+    node_t *temp = polynome->head;
+    while (temp)
+    {
+        temp->coeff *= temp->pow;
+        temp->pow -= 1;
+        temp = temp->next;
+    }
+}
+
+void devide_pol_to_odd_even(ll_polynome_t *polynome, ll_polynome_t *odd_nodes, ll_polynome_t *even_nodes)
+{
+    node_t *temp;
+    while (polynome->head)
+    {
+        temp = polynome->head;
+        polynome->head = polynome->head->next;
+        temp->next = NULL;
+        if (temp->pow % 2 == 0)
+            push(even_nodes, temp);
+        else
+            push(odd_nodes, temp);
+    }
+}
+
+ll_polynome_t *polynome_addition(ll_polynome_t *lhs, ll_polynome_t *rhs)
+{
+    ll_polynome_t *sum = NULL;
+    if (create_ll_node(&sum) == ok)
+    {
+        node_t *temp;
+        while (lhs->head && rhs->head)
+        {
+            if (lhs->head->pow > rhs->head->pow)
+            {
+                temp = lhs->head;
+                lhs->head = lhs->head->next;
+                temp->next = NULL;
+                push(sum, temp);
+            }
+            else if (lhs->head->pow < rhs->head->pow)
+            {
+                temp = rhs->head;
+                rhs->head = rhs->head->next;
+                temp->next = NULL;
+                push(sum, temp);
+            }
+            else
+            {
+                temp = lhs->head;
+                lhs->head->coeff += rhs->head->coeff;
+                lhs->head = lhs->head->next;
+                temp->next = NULL;
+                push(sum, temp);
+                temp = rhs->head;
+                rhs->head = rhs->head->next;
+                free(temp);
+            }
+        }
+        if (lhs->head)
+            sum->end = lhs->head;
+        else if (rhs->head)
+            sum->end = rhs->head;
+    }
+    return sum;
+}
 
 int main()
-{    
-    int rc = error;
-    string_t *choice;
-    choice = get_line();
+{
+    char *choice = get_string();
+    int rc = ok;
     if (choice)
     {
-        if (strcmp(choice->string, POLYNOM_VALUE) == ok)
+        if (strcmp(choice, "val") == ok || strcmp(choice, "ddx") == ok || strcmp(choice, "dvd") == ok)
         {
-            poly_node *list;
-            init_list_node(&list);
-            
-            int val;
-            if (list)
+            ll_polynome_t *polynome;
+            char *poly_coeff_pow = get_string();
+            int x_value;
+            if (poly_coeff_pow && create_ll_node(&polynome) == ok)
             {
-                string_t *polynom_coeffs = get_line();
-                if (polynom_coeffs)
+                if (create_list_from_line(poly_coeff_pow, polynome) == error)
+                    rc = error;
+                else
                 {
-                    rc = create_list_from_line(polynom_coeffs, list);
-                    if (rc == ok)
+                    if (strcmp(choice, "val") == ok)
                     {
-                        if (fscanf(stdin, "%d", &val) == 1)
-                        {
-                            printf("%ld\n", get_polynom_val(list, val));
-                            free_list(list);
-                        }
+                        if (fscanf(stdin, "%d", &x_value) == 1)
+                            printf("%ld\n", polynome_val(polynome, x_value));
                         else
                             rc = error;
                     }
-                    free_string(polynom_coeffs);
-                }
-            }
-        }
-        else if (strcmp(choice->string, POLYNOM_DERIVATIVE) == ok)
-        {
-            poly_node *list;
-            init_list_node(&list);
-            if (list)
-            {
-                string_t *polynom_coeffs = get_line();
-                if (polynom_coeffs)
-                {
-                    rc = create_list_from_line(polynom_coeffs, list);
-                    if (rc == ok)
+                    else if (strcmp(choice, "dvd") == ok)
                     {
-                        dirvative_of_poly(list);
-                        print_list(list);
-                    }
-                    free_string(polynom_coeffs);
-                }
-                free_list(list);
-            }
-        }
-        else if (strcmp(choice->string, POLYNOM_ADDITION) == ok)
-        {
-            poly_node *list_rhs, *list_lhs;
-            init_list_node(&list_rhs);
-            init_list_node(&list_lhs);
-            if (list_rhs && list_lhs)
-            {
-                string_t *first_poly = get_line();
-                string_t *second_poly = get_line();
-                if (first_poly && second_poly)
-                {
-                    if (create_linked_list(first_poly, second_poly, list_lhs, list_rhs) == ok)
-                    {
-                        poly_node *sum = sum_of_polys(list_lhs, list_rhs);
-                        if (sum)
+                        ll_polynome_t *odd_nodes, *even_nodes;
+                        odd_nodes = even_nodes = NULL;
+                        if (create_ll_node(&odd_nodes) == ok && create_ll_node(&even_nodes) == ok)
                         {
-                            print_sum_of_polys(sum);
-                            free_list(sum);
-                            rc = ok;
+                            devide_pol_to_odd_even(polynome, odd_nodes, even_nodes);
+                            if (even_nodes->head)
+                                print_list(even_nodes, just_print);
+                            if (odd_nodes->head)
+                                print_list(odd_nodes, just_print);
                         }
-                        free_list(list_rhs);
-                        free_list(list_lhs); 
+                        else
+                        {
+                            rc = error;
+                            if (odd_nodes)
+                                free_list(odd_nodes);
+                            if (even_nodes)
+                                free_list(even_nodes);
+                        }
                     }
-                    free_string(first_poly);
-                    free_string(second_poly);
+                    else
+                    {
+                        derivative_if_polynome(polynome);
+                        print_list(polynome, derivative);
+                    }
                 }
+                free(poly_coeff_pow);
+                free_list(polynome);
+            }
+            else
+            {
+                free(poly_coeff_pow);
+                rc = error;
+            }
+        }
+        else if (strcmp(choice, "sum") == ok)
+        {
+            char *lhs_coeff_and_pow = get_string();
+            char *rhs_coeff_and_pow = get_string();
+            ll_polynome_t *lhs_polynome, *rhs_polynome;
+            if (lhs_coeff_and_pow && rhs_coeff_and_pow && create_ll_node(&lhs_polynome) == ok && create_ll_node(&rhs_polynome) == ok)
+            {
+                if (create_list_from_line(lhs_coeff_and_pow, lhs_polynome) == error || create_list_from_line(rhs_coeff_and_pow, rhs_polynome) == error)
+                    rc = error;
                 else
                 {
-                    free_string(first_poly);
-                    free_string(second_poly);
+                    ll_polynome_t *sum = polynome_addition(lhs_polynome, rhs_polynome);
+                    print_list(sum, sum_print);
+                    free_list(sum);
                 }
+                free_list(lhs_polynome);
+                free_list(rhs_polynome);
+                free(lhs_coeff_and_pow);
+                free(rhs_coeff_and_pow);
             }
             else
             {
-                free_list(list_lhs);
-                free_list(list_rhs);
+                rc = error;
+                if (lhs_coeff_and_pow)
+                    free(lhs_coeff_and_pow);
+                if (rhs_coeff_and_pow)
+                    free(rhs_coeff_and_pow);
+                free_list(lhs_polynome);
+                free_list(rhs_polynome);
             }
         }
-        else if (strcmp(choice->string, POLYNOM_DIVISION) == ok)
-        {
-            poly_node *list;
-            poly_node *odd_nodes;
-            poly_node *even_nodes;
-            init_list_node(&list);
-            init_list_node(&odd_nodes);
-            init_list_node(&even_nodes);
-            
-            if (list && odd_nodes && even_nodes)
-            {
-                string_t *polynom_coeffs = get_line();
-                if (polynom_coeffs)
-                {
-                    rc = create_list_from_line(polynom_coeffs, list);
-                    if (rc == ok)
-                    {
-                        create_even_odd_nodes(list, odd_nodes, even_nodes);
-                        print_nodes(even_nodes);
-                        print_nodes(odd_nodes);
-                    }
-                    free_string(polynom_coeffs);
-                }
-                free_list(even_nodes);
-                free_list(odd_nodes);
-                free_list(list);
-            }
-            else
-            {
-                free_list(list);
-                free_list(odd_nodes);
-                free_list(even_nodes);
-            }
-        }
-        free_string(choice);
+        free(choice);
     }
     return rc;
 }
