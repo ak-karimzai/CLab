a67d1e1392a08fdaf729f3d39860174290b9692f
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2020-11-28 21:51:13 +0300

    checked but problem not findedmake

--- lab_11_1_1/main.c
+++ lab_11_1_1/main.c
@@ -5,7 +5,7 @@
 #include <inttypes.h>
 
 int my_snprintf(char *str_s, size_t n, const char *str_format, ...);
-bool check_fun(const char *fmt, int num)
+bool check_fun(const char *fmt, long int num)
 {
     bool result = true;
     int size = snprintf(NULL, 0, fmt, num) + 2;
@@ -18,8 +18,8 @@ bool check_fun(const char *fmt, int num)
         int my_write = my_snprintf(my_buf, i, fmt, num);
         if (write != my_write || strcmp(buffer, my_buf) != 0)
         {
-            printf("   snprintf(bif, %d, \"%s\", %d) = %d, buf = \"%s\"\n", i, fmt, num, write, buffer);
-            printf("my_snprintf(bif, %d, \"%s\", %d) = %d, buf = \"%s\"\n", i, fmt, num, my_write, my_buf);
+            printf("   snprintf(bif, %d, \"%s\", %ld) = %d, buf = \"%s\"\n", i, fmt, num, write, buffer);
+            printf("my_snprintf(bif, %d, \"%s\", %ld) = %d, buf = \"%s\"\n", i, fmt, num, my_write, my_buf);
             result = false;
         }
     }
@@ -33,12 +33,10 @@ bool check_fun(const char *fmt, int num)
 
 int main()
 {
-    // check_fun("%d", 0);
-    // check_fun("%d", 0);
-    char arr[20];
-    my_snprintf(arr, 20, "%lld", 4000000000);
-    puts(arr);
-    printf("%lu\n", sizeof(long));
-    printf("%lu\n", sizeof(int));
+    check_fun("%ld", 12321);
+    check_fun("%ld", 12321);
+    printf("%d\n", sizeof(long) == sizeof(int64_t));
+    // char arr[20];
+    printf("%d\n", sizeof(int) == sizeof(int32_t));
     return 0;
 }
