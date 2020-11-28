662edde6abc72474e2af4ad5e12881edccbb30b6
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2020-11-28 20:32:13 +0300

    checked but problem not findedmake

--- lab_11_1_1/main.c
+++ lab_11_1_1/main.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
-int decimal_to_octal(int decimal)
+int my_snprintf(char *str_s, size_t n, const char *str_format, ...);
+bool check_fun(const char *fmt, int num)
 {
-    int res = 0, mul = 1, last;
-    double baqi;
-    while (decimal)
+    bool result = true;
+    int size = snprintf(NULL, 0, fmt, num) + 2;
+    char *buffer = calloc(size, sizeof(char));
+    char *my_buf = calloc(size, sizeof(char));
+
+    for (int i = 0; i < size; i++)
     {
-        baqi = (double) decimal / 8;
-        decimal = (int) baqi;
-        baqi -= decimal;
-        last = (int) 8 * baqi;
-        res += (last * mul);
-        mul *= 10;
+        int write = snprintf(buffer, i, fmt, num);
+        int my_write = my_snprintf(my_buf, i, fmt, num);
+        if (write != my_write || strcmp(buffer, my_buf) != 0)
+        {
+            printf("   snprintf(bif, %d, \"%s\", %d) = %d, buf = \"%s\"\n", i, fmt, num, write, buffer);
+            printf("my_snprintf(bif, %d, \"%s\", %d) = %d, buf = \"%s\"\n", i, fmt, num, my_write, my_buf);
+            result = false;
+        }
     }
-    return res;
-}
-
-int print_a_int(long int x)
-{
-    printf("%ld", x);
+    if (result == true)
+        printf("passed for all %d \n", size);
+    
+    free(buffer);
+    free(my_buf);
+    return result;
 }
 
 int main()
 {
-    
+    // check_fun("%d", 0);
+    // check_fun("%d", 0);
+    char arr[20];
+    my_snprintf(arr, 20, "%ld", 10000000000000);
+    puts(arr);
+    return 0;
 }
--- lab_11_1_1/temp.c
+++ lab_11_1_1/temp.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <stdarg.h>
-#include <string.h>
-#include <stdbool.h>
+
+
 size_t my_strlen(const char *str)
 {
     size_t len = 0;
@@ -159,39 +159,3 @@ int my_snprintf(char *str_s, size_t n, const char *str_format, ...)
     va_end(args);
     return str_index;
 }
-
-bool check_fun(const char *fmt, int num)
-{
-    bool result = true;
-    int size = snprintf(NULL, 0, fmt, num) + 2;
-    char *buffer = calloc(size, sizeof(char));
-    char *my_buf = calloc(size, sizeof(char));
-
-    for (int i = 0; i < size; i++)
-    {
-        int write = snprintf(buffer, i, fmt, num);
-        int my_write = my_snprintf(my_buf, i, fmt, num);
-        if (write != my_write || strcmp(buffer, my_buf) != 0)
-        {
-            printf("   snprintf(bif, %d, \"%s\", %d) = %d, buf = \"%s\"\n", i, fmt, num, write, buffer);
-            printf("my_snprintf(bif, %d, \"%s\", %d) = %d, buf = \"%s\"\n", i, fmt, num, my_write, my_buf);
-            result = false;
-        }
-    }
-    if (result == true)
-        printf("passed for all %d \n", size);
-    
-    free(buffer);
-    free(my_buf);
-    return result;
-}
-
-// int main()
-// {
-//     // check_fun("%d", 0);
-//     // check_fun("%d", 0);
-//     char arr[20];
-//     my_snprintf(arr, 20, "%ld", 10000000000000);
-//     puts(arr);
-//     return 0;
-// }
