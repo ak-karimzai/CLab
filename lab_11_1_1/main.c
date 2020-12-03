feb2f9ca4ac23b54b7ef6e012bd7b0662bfd6428
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2020-12-03 08:53:19 +0300

    need to change

--- lab_11_1_1/main.c
+++ lab_11_1_1/main.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
-#include <inttypes.h>
+#include "./inc/main.h"
 
-int my_snprintf(char *str_s, size_t n, const char *str_format, ...);
 bool check_fun(const char *fmt, long num)
 {
     bool result = true;
@@ -31,12 +26,73 @@ bool check_fun(const char *fmt, long num)
     return result;
 }
 
+bool check_fun_char(const char *fmt, char num)
+{
+    bool result = true;
+    int size = snprintf(NULL, 0, fmt, num) + 2;
+    char *buffer = calloc(size, sizeof(char));
+    char *my_buf = calloc(size, sizeof(char));
+
+    for (int i = 0; i < size; i++)
+    {
+        int write = snprintf(buffer, i, fmt, num);
+        int my_write = my_snprintf(my_buf, i, fmt, num);
+        if (write != my_write || strcmp(buffer, my_buf) != 0)
+        {
+            printf("   snprintf(bif, %d, \"%s\", %c) = %d, buf = \"%s\"\n", i, fmt, num, write, buffer);
+            printf("my_snprintf(bif, %d, \"%s\", %c) = %d, buf = \"%s\"\n", i, fmt, num, my_write, my_buf);
+            result = false;
+        }
+    }
+    if (result == true)
+        printf("passed for all %d \n", size);
+    
+    free(buffer);
+    free(my_buf);
+    return result;
+}
+
+bool check_fun_str(const char *fmt, char *num)
+{
+    bool result = true;
+    int size = snprintf(NULL, 0, fmt, num) + 2;
+    char *buffer = calloc(size, sizeof(char));
+    char *my_buf = calloc(size, sizeof(char));
+
+    for (int i = 0; i < size; i++)
+    {
+        int write = snprintf(buffer, i, fmt, num);
+        int my_write = my_snprintf(my_buf, i, fmt, num);
+        if (write != my_write || strcmp(buffer, my_buf) != 0)
+        {
+            printf("   snprintf(bif, %d, \"%s\", %s) = %d, buf = \"%s\"\n", i, fmt, num, write, buffer);
+            printf("my_snprintf(bif, %d, \"%s\", %s) = %d, buf = \"%s\"\n", i, fmt, num, my_write, my_buf);
+            result = false;
+        }
+    }
+    if (result == true)
+        printf("passed for all %d \n", size);
+    
+    free(buffer);
+    free(my_buf);
+    return result;
+}
+
 int main()
 {
-    check_fun("%d", 12321);
-    check_fun("%d", 12321);
-    printf("%d\n", sizeof(long) == sizeof(int64_t));
-    // char arr[20];
-    printf("%d\n", sizeof(int) == sizeof(int32_t));
+    check_fun("%d", -2000000000);
+    check_fun("%d", 123456789);
+    check_fun("%d", 0);
+    check_fun("%d", 2000000000);
+    check_fun("%d", -123456789);
+
+    check_fun_char("%c", 'c');
+    check_fun_char("%c", 'A');
+    check_fun_char("%c", 'f');
+
+    check_fun_str("%s", "Ahmad Khalid Karimzai");
+    check_fun_str("%s", "Just for testing");
+    check_fun_str("%s", "??????????????");
+
     return 0;
 }
